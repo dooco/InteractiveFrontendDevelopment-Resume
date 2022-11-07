132d089037882c901839506cc15d7d08638a8b35
dooco <padraigdooley@gmail.com>
2022-11-07 21:57:58 +0000

    update contact

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -36,7 +36,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
-
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2> Please enter a GutHub username</h2> `);
@@ -59,10 +60,14 @@ function fetchGitHubInformation(event) {
         function (errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
-                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>}`);
+                    `< h2 > Error: ${errorResponse.responseJSON.message}</h2 >}`);
             }
         });
 }
+$(document).ready(fetchGitHubInformation);
