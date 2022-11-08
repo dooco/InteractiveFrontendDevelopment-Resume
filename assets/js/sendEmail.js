9ca46524abe30bb159c769345f206280c9442abb
dooco <padraigdooley@gmail.com>
2022-11-08 21:36:08 +0000

    set up and debug emailJS

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,18 @@
-console.log("Hello Contact send email");
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie-contact", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+        .then(function (response) {
+            console.log("SUCCESS", response);
+        },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false;  // To block from loading a new page
+}
+        // "from_name": contactForm.name.value,
+        // "from_email": contactForm.emailaddress.value,
+        // "project_request": contactForm.projectsummary.value
